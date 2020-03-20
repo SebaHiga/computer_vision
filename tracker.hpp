9ebd187795d746fe7a12dd85a9845caf724fa67c
Sebastian <sebastianariel.higa@globant.com>
2020-03-20 18:05:09 -0300

    pointer optimization

--- tracker.hpp
+++ tracker.hpp
@@ -8,8 +8,10 @@
 using namespace std;
 using namespace geom;
 
-#define MAX_DISS 30
-#define MIN_APP 5
+#define MAX_DISS 20
+#define MIN_APP 4
+#define MAX_DOTS 30
+#define MAX_RANGE 55
 
 //max range
 
@@ -25,14 +27,16 @@ public:
     geom::Point position;
     geom::Point position_old;
 
-    geom::Vector speed;
+    std::vector<geom::Vector> speed;
+
+    std::vector<geom::Point> trackline;
 
     Object();
 
     Object(int id, geom::Point pos) :   id(id), position(pos), updated(true),
                                         maxDiss(MAX_DISS), minAppearance(MIN_APP), valid(false){
-        speed.module = 0;
-        speed.angle = 0;
+        speed.push_back(geom::Vector());
+        trackline.push_back(pos);
     }
 
     ~Object(){};
@@ -42,10 +46,12 @@ public:
                 maxDiss--;
                 position_old = position;
 
-                position.update(speed);
-
-                if(speed.module){
-                    speed.module *= 1;
+                speed[speed.size()].module *= 0.5;
+                position.update(speed[speed.size()]);
+                trackline.push_back(position);
+                
+                if(trackline.size() > MAX_DOTS){
+                    trackline.erase(trackline.begin());
                 }
 
                 return false;
@@ -58,12 +64,22 @@ public:
     }
 
     void update(geom::Point p){
-        geom::Vector vel = position.getVelocity(p);
+        geom::Vector vel = position.getVelocity(p), accumulator;
         
-        vel.module = vel.module;
-        vel.angle = (vel.angle + speed.angle)/2;        
+        for(int i = 0; i < speed.size(); i++){
+            accumulator += speed[i];
+        }
+        accumulator += vel;
 
-        speed = vel;
+        if(speed.size()){
+            accumulator /= speed.size() + 1;
+        }
+
+        speed.push_back(accumulator);
+
+        if(speed.size() > 3){
+            speed.erase(speed.begin());
+        }
 
         position_old = position;
         position = p;
@@ -71,6 +87,11 @@ public:
         updated = true;
 
         maxDiss = MAX_DISS;
+
+        trackline.push_back(p);
+        if(trackline.size() > MAX_DOTS){
+            trackline.erase(trackline.begin());
+        }
 
         if(minAppearance){
             minAppearance--;
@@ -81,14 +102,14 @@ public:
     }
 
     geom::Point getSpeedPoint(){
-        return geom::Point(position.top + speed.module * sin(speed.angle) * 2,
-                            position.left + speed.module * cos(speed.angle) * 2);
+        return geom::Point(position.top + speed[speed.size()].module * sin(speed[speed.size()].angle) * 10,
+                            position.left + speed[speed.size()].module * cos(speed[speed.size()].angle) * 10);
     }
 };
 
 class Tracker {
 private:
-    static const int maxRange = 250;
+    static const int maxRange = MAX_RANGE;
 
 public:
     vector<Object> objects;
@@ -96,26 +117,26 @@ public:
     ~Tracker(){};
 
     // returns the total difference of tracked objects after updating
-    int update(vector<geom::Point> points);
+    int update(vector<geom::Point> *points);
     void addObject(geom::Point p);
     int getNewID();
 };
 
 
-int Tracker::update(vector<geom::Point> points){
+int Tracker::update(vector<geom::Point> *points){
     vector<Object> old_objects = objects;
 
     for (int i = 0; i < objects.size(); i++){
         objects[i].updated = false;
     }
 
-    for (int i = 0; i < points.size(); i++){
-        points[i].updated = false;
+    for (int i = 0; i < points->size(); i++){
+        (*points)[i].updated = false;
     }
 
     // valid priority analysis
-    for (int i = 0; i < points.size(); i++){
-        if(points[i].updated){
+    for (int i = 0; i < points->size(); i++){
+        if((*points)[i].updated){
             continue;
         }
 
@@ -125,7 +146,7 @@ int Tracker::update(vector<geom::Point> points){
         // find minimun distance for each updated object
         for(int j = 0; j < objects.size(); j++){
             if(!objects[j].updated && objects[j].valid){
-                dist = points[i].getDistance(objects[j].position);
+                dist = (*points)[i].getDistance(objects[j].position);
 
                 if (dist < minDist){
                     minDist = dist;
@@ -136,14 +157,14 @@ int Tracker::update(vector<geom::Point> points){
 
         // refresh positions or add objects
         if(minDist < maxRange){
-            objects[index].update(points[i]);
-            points[i].updated = true;
+            objects[index].update((*points)[i]);
+            (*points)[i].updated = true;
         }
     }
 
     // invalid object analysis
-    for (int i = 0; i < points.size(); i++){
-        if(points[i].updated){
+    for (int i = 0; i < points->size(); i++){
+        if((*points)[i].updated){
             continue;
         }
 
@@ -153,7 +174,7 @@ int Tracker::update(vector<geom::Point> points){
         // find minimun distance for each updated object
         for(int j = 0; j < objects.size(); j++){
             if(!objects[j].updated && !objects[j].valid){
-                dist = points[i].getDistance(objects[j].position);
+                dist = (*points)[i].getDistance(objects[j].position);
 
                 if (dist < minDist){
                     minDist = dist;
@@ -164,12 +185,12 @@ int Tracker::update(vector<geom::Point> points){
 
         // refresh positions or add objects
         if(minDist < maxRange){
-            objects[index].update(points[i]);
+            objects[index].update((*points)[i]);
         }
         else{
-            addObject(points[i]);
+            addObject((*points)[i]);
         }
-        points[i].updated = true;
+        (*points)[i].updated = true;
     }
 
     // compute lost objects
